549c62e72470011f7ea39f7a3fd824367790144b
Alexis Victor Nicolas Bagia <alexis@bagia.de>
2022-12-23 18:34:51 +0100

    add triggers for more relaistic testing

---
+++ ref/hal.c
@@ -0,0 +1,6 @@
+//
+// Created by Alexis Bagia on 22.12.22.
+//
+#include "hal.h"
+void trigger_low(void) {}
+void trigger_high(void) {}
---
+++ ref/hal.h
@@ -0,0 +1,10 @@
+//
+// Created by Alexis Bagia on 22.12.22.
+//
+
+#ifndef DILITHIUM_HAL_H
+#define DILITHIUM_HAL_H
+
+void trigger_low(void);
+void trigger_high(void);
+#endif //DILITHIUM_HAL_H
